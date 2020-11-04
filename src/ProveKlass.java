12ceb0712983b98c6bdbb4c6d20a9aa180889668
jonhagos <jon4geo@gmail.com>
2020-11-04 12:22:28 +0100

    prove klass

---
+++ src/ProveKlass.java
@@ -0,0 +1,8 @@
+public class ProveKlass {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 9;
+        int c = a + b;
+        System.out.println("The total sum is "+c);
+    }
+}
